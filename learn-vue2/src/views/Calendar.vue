d2263b6993597829f559022c3db36fc480f79257
gakuy <gaku.yoko518@outlook.jp>
2021-07-22 21:00:56 +0900

    成果0722

--- learn-vue2/src/views/Calendar.vue
+++ learn-vue2/src/views/Calendar.vue
@@ -1,6 +1,68 @@
 <template>
   <v-row justify="center">
-    <v-date-picker v-model="picker"></v-date-picker>
+    <div>
+      <v-sheet
+          class="d-flex"
+          height="54"
+          tile
+      >
+        <v-btn
+            class="ma-2"
+            icon
+            @click="$refs.calendar.prev()"
+        >
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-select
+            v-model="type"
+            :items="types"
+            class="ma-2"
+            dense
+            hide-details
+            label="type"
+            outlined
+        ></v-select>
+        <v-select
+            v-model="mode"
+            :items="modes"
+            class="ma-2"
+            dense
+            hide-details
+            label="event-overlap-mode"
+            outlined
+        ></v-select>
+        <v-select
+            v-model="weekday"
+            :items="weekdays"
+            class="ma-2"
+            dense
+            hide-details
+            label="weekdays"
+            outlined
+        ></v-select>
+        <v-spacer></v-spacer>
+        <v-btn
+            class="ma-2"
+            icon
+            @click="$refs.calendar.next()"
+        >
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-sheet>
+      <v-sheet height="600">
+        <v-calendar
+            ref="calendar"
+            v-model="value"
+            :event-color="getEventColor"
+            :event-overlap-mode="mode"
+            :event-overlap-threshold="30"
+            :events="events"
+            :type="type"
+            :weekdays="weekday"
+            @change="getEvents"
+        ></v-calendar>
+      </v-sheet>
+    </div>
   </v-row>
 </template>
 
@@ -8,14 +70,60 @@
 
 export default {
   name: "Calendar",
-  data() {
-    return {
-      picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
-    }
-  },
+  data: () => ({
+    type: 'month',
+    types: ['month', 'week', 'day', '4day'],
+    mode: 'stack',
+    modes: ['stack', 'column'],
+    weekday: [0, 1, 2, 3, 4, 5, 6],
+    weekdays: [
+      {text: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6]},
+      {text: 'Mon - Sun', value: [1, 2, 3, 4, 5, 6, 0]},
+      {text: 'Mon - Fri', value: [1, 2, 3, 4, 5]},
+      {text: 'Mon, Wed, Fri', value: [1, 3, 5]},
+    ],
+    value: '',
+    events: [],
+    colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
+    names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
+  }),
   methods: {
+    getEvents({start, end}) {
+      const events = []
+
+      const min = new Date(`${start.date}T00:00:00`)
+      const max = new Date(`${end.date}T23:59:59`)
+      const days = (max.getTime() - min.getTime()) / 86400000
+      const eventCount = this.rnd(days, days + 20)
+
+      for (let i = 0; i < eventCount; i++) {
+        const allDay = this.rnd(0, 3) === 0
+        const firstTimestamp = this.rnd(min.getTime(), max.getTime())
+        const first = new Date(firstTimestamp - (firstTimestamp % 900000))
+        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
+        const second = new Date(first.getTime() + secondTimestamp)
+
+        events.push({
+          name: this.names[this.rnd(0, this.names.length - 1)],
+          start: first,
+          end: second,
+          color: this.colors[this.rnd(0, this.colors.length - 1)],
+          timed: !allDay,
+        })
+      }
+
+      this.events = events
+    },
+    getEventColor(event) {
+      return event.color
+    },
+    rnd(a, b) {
+      return Math.floor((b - a + 1) * Math.random()) + a
+    },
   },
-};</script>
+}
+</script>
+
 
 <style scoped>
 
